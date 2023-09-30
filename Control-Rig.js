7c1856b66786fa238a257a1ffbbb00fa3a6923db
catrone3 <catrone3@gmail.com>
2023-09-30 18:31:22 -0400

    fix: combined control-rig and jacked-in as you need the control-rig to jack in

--- Control-Rig.js
+++ Control-Rig.js
@@ -6,7 +6,12 @@ for (let key=0; key<items.length; key++) {
     rating = items[key].system.technology.rating;
   };
 }
-
+let logic = player.system.attributes.logic.value;
+let intuition = player.system.attributes.intuition.value;
+let reaction = player.system.attributes.reaction.value;
+let firewall = player.system.attributes.firewall.value;
+let data_processing = player.system.attributes.data_processing.value;
+let init = player.system.initiative.matrix.base.value;
 let gunnery = player.system.skills.active.gunnery.value + rating;
 let pilot_ground = player.system.skills.active.pilot_ground_craft.value + rating;
 let pilot_aero = player.system.skills.active.pilot_aerospace.value + rating;
@@ -18,9 +23,15 @@ effect.update({
   label: "Control Rig Bonus",
     changes:
     [
-      {key:"system.vehicle_stats.handling.temp", value: rating, mode: "2"},
+      {key:"system.vehicle_stats.handling.temp", value: rating, mode: 2},
+      {key:"system.attributes.logic.value", value: logic, mode:5},
+      {key:"system.attributes.intuition.value", value: intuition, mode:5},
+      {key:"system.attributes.reaction.value", value: reaction, mode:5},
+      {key:"system.attributes.firewall.value", value: firewall, mode:5},
+      {key:"system.attributes.data_processing.value", value: data_processing, mode:5},
+      {key:"system.initiative.matrix.base.value", value: init, mode:5},
+      {key:"system.initiative.meatspace.base.value", value: init, mode:5},
       {key:"system.vehicle_stats.speed.temp", value: rating, mode: 2},
-      {key:"system.skills.active.gunnery.value", value: rating, mode:2},
       {key:"system.skills.active.gunnery.value", value: gunnery, mode:5},
       {key:"system.skills.active.pilot_ground_craft.value", value: pilot_ground, mode:5},
       {key:"system.skills.active.pilot_aerospace.value", value: pilot_aero, mode:5},
